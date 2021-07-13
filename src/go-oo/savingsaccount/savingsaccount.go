5ac6228bb868a50f4dd5be6403fe228b9c1ee0f7
ericklima-ca <73451993+ericklima-ca@users.noreply.github.com>
2021-07-12 23:26:30 -0400

    Create savingsaccount.go

---
+++ src/go-oo/savingsaccount/savingsaccount.go
@@ -0,0 +1,37 @@
+package savingsaccount
+
+import (
+	"fmt"
+	"go-oo/customer"
+)
+
+type SavingsAccount struct {
+	Holder    customer.Holder
+	Branch    int
+	Account   int
+	Operation int
+	balance   float64
+}
+
+func (c *SavingsAccount) Withdraw(cash float64) string {
+	if cash <= c.balance && cash > 0 {
+		c.balance -= cash
+		return fmt.Sprintf("%.2f successfully withdrawn. Yor new balance is: %.2f.", cash, c.balance)
+	} else {
+		return fmt.Sprintf("Withdraw failed. Your balance is only %.2f.", c.balance)
+	}
+}
+
+func (c *SavingsAccount) Deposit(cash float64) string {
+	if cash > 0 {
+		c.balance += cash
+		return fmt.Sprintf("%.2f deposited successfully. Your new balance is: %.2f.",
+			cash, c.balance)
+	} else {
+		return "Deposit failed."
+	}
+}
+
+func (c *SavingsAccount) Statement() string {
+	return fmt.Sprintf("Your current balance is %.2f", c.balance)
+}
